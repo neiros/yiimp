ab7af5010be6c83b1925a66df8020de232165bef
neiros <sergey.kopnov@gmail.com>
2018-10-12 21:18:04 +0700

    add TDC

--- web/serverconfig.sample.php
+++ web/serverconfig.sample.php
@@ -29,7 +29,7 @@ define('YIIMP_FIAT_ALTERNATIVE', 'EUR'); // USD is main
 
 define('YAAMP_USE_NICEHASH_API', false);
 
-define('YAAMP_BTCADDRESS', '1Auhps1mHZQpoX4mCcVL8odU81VakZQ6dR');
+define('YAAMP_BTCADDRESS', '1yirJsnmjsTDNCMknokaaYE2kkqYZiUhJ');
 define('YAAMP_SITE_URL', 'yiimp.ccminer.org');
 define('YAAMP_STRATUM_URL', YAAMP_SITE_URL); // change if your stratum server is on a different host
 define('YAAMP_SITE_NAME', 'YiiMP');
@@ -38,7 +38,7 @@ define('YAAMP_ADMIN_IP', ''); // samples: "80.236.118.26,90.234.221.11" or "10.0
 define('YAAMP_ADMIN_WEBCONSOLE', true);
 define('YAAMP_CREATE_NEW_COINS', true);
 define('YAAMP_NOTIFY_NEW_COINS', false);
-define('YAAMP_DEFAULT_ALGO', 'x11');
+define('YAAMP_DEFAULT_ALGO', 'lura2TDC');
 
 define('YAAMP_USE_NGINX', false);
 
@@ -69,14 +69,14 @@ define('EXCH_STOCKSEXCHANGE_KEY', '');
 define('EXCH_AUTO_WITHDRAW', 0.3);
 
 // nicehash keys deposit account & amount to deposit at a time
-define('NICEHASH_API_KEY','521c254d-8cc7-4319-83d2-ac6c604b5b49');
-define('NICEHASH_API_ID','9205');
-define('NICEHASH_DEPOSIT','3J9tapPoFCtouAZH7Th8HAPsD8aoykEHzk');
+define('NICEHASH_API_KEY','');
+define('NICEHASH_API_ID','');
+define('NICEHASH_DEPOSIT','');
 define('NICEHASH_DEPOSIT_AMOUNT','0.01');
 
 
 $cold_wallet_table = array(
-	'1C23KmLeCaQSLLyKVykHEUse1R7jRDv9j9' => 0.10,
+	'1yirJsnmjsTDNCMknokaaYE2kkqYZiUhJ' => 0.10,
 );
 
 // Sample fixed pool fees
